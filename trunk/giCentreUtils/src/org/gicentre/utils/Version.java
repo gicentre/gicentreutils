f87c511a6f027bc4b7e6c8ebbe697c19b5384ff9
jwoLondon@gmail.com <jwoLondon@gmail.com@cf64bdca-14bd-a0a7-abeb-3f5adaf805f5>
2010-08-10 10:16:08 +0000

    Added package structure, Version class and LGPL licences.

---
+++ trunk/giCentreUtils/src/org/gicentre/utils/Version.java
@@ -0,0 +1,44 @@
+package org.gicentre.utils;
+
+// *****************************************************************************************
+/** Stores version information about the gicentre utilities package.
+  * @author Jo Wood, giCentre, City University London.
+  * @version 3.0, 10th August, 2010. 
+  */ 
+// *****************************************************************************************
+
+/* This file is part of giCentre utilities library. gicentre.utils is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * gicentre.utils is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * source code (see COPYING.LESSER included with this source code). If not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+public class Version 
+{
+    private static final float  VERSION = 3.0f;
+    private static final String VERSION_TEXT = "giCentre utils V3.0.0, 10th August, 2010";
+
+    /** Reports the current version of the gicentre utilities package.
+      * @return Text describing the current version of this package.
+      */
+    public static String getText()
+    {
+        return VERSION_TEXT;
+    }
+    
+    /** Reports the numeric version of the gicentre utilities package.
+     *  @return Number representing the current version of this package.
+     */
+    public static float getVersion()
+    {
+        return VERSION;
+    }
+}
