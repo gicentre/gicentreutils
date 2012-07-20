86330f461e5854c614d485e46785924966257105
jwoLondon@gmail.com <jwoLondon@gmail.com@cf64bdca-14bd-a0a7-abeb-3f5adaf805f5>
2012-07-20 17:03:11 +0000

    Updated javadoc to reflect latest changes.

--- trunk/giCentreUtils/src/org/gicentre/utils/text/WrappedToken.java
+++ trunk/giCentreUtils/src/org/gicentre/utils/text/WrappedToken.java
@@ -8,12 +8,11 @@ import processing.core.PGraphics;
 
 //****************************************************************************************
 /** Holds details of wrapped tokens from org.gicentre.utils.text.wordWrapAndTokenise()
- * and can return the screen bounds of the token, using the current textFont, textSize
- * and textAlign.
-*  
-* @author Aidan Slingsby, giCentre, City University London.
-* @version 1.0, August 2011 
-*/ 
+ *  and can return the screen bounds of the token, using the current textFont, textSize
+ *  and textAlign.
+ *  @author Aidan Slingsby, giCentre, City University London.
+ *  @version 1.1, July 2012 
+ */ 
 //*****************************************************************************************
 
 /* This file is part of giCentre utilities library. gicentre.utils is free software: you can 
@@ -41,43 +40,43 @@ public class WrappedToken {
 		return id+": "+text+" "+x+","+y;
 	}
 	
-	/** Returns a rectangle describing the screen bounds of this token (if drawn
-	 * correctly)
-	 * 
-	 * Corrects for the different textAlign modes of the sketch
-	 * 
-	 * @param applet
-	 * @return
+	/** Returns a rectangle describing the screen bounds of this token (if drawn correctly)
+	 *  Corrects for the different textAlign modes of the sketch.
+	 *  @param sketch Sketch doing the drawing.
+	 *  @return Bounds of this token in screen coordinates.
 	 */
 	public Rectangle getBounds(PApplet sketch){
 		return getBounds(sketch.g);
 	}
 
-	/** Returns a rectangle describing the screen bounds of this token (if drawn
-	 * correctly)
-	 * 
-	 * Corrects for the different textAlign modes of the pGraphics
-	 * 
-	 * @param applet
-	 * @return
+	/** Returns a rectangle describing the screen bounds of this token (if drawn correctly)
+	 *  Corrects for the different textAlign modes of the pGraphics.
+	 *  @param pGraphics The graphics context within which the text drawing takes place.
+	 *  @return Bounds of this token in screen coordinates.
 	 */
 	public Rectangle getBounds(PGraphics pGraphics){
 		float w=pGraphics.textWidth(this.text);
 		float h=pGraphics.textAscent()+pGraphics.textDescent();
-		float x=this.x;
-		float y=this.y;
+		float xPos=this.x;
+		float yPos=this.y;
 		
 		if (pGraphics.textAlignY==PConstants.CENTER)
-			y-=(h/2);
+		{
+			yPos-=(h/2);
+		}
 		else if (pGraphics.textAlignY==PConstants.BOTTOM)
-			y-=h;
+		{
+			yPos-=h;
+		}
 
 		if (pGraphics.textAlign==PConstants.CENTER)
-			x-=w/2;
+		{
+			xPos-=w/2;
+		}
 		else if (pGraphics.textAlign==PConstants.RIGHT)
-			x-=w;
-	
-		return new Rectangle(Math.round(x),Math.round(y),Math.round(w),Math.round(h));
-		
+		{
+			xPos-=w;
+		}
+		return new Rectangle(Math.round(xPos),Math.round(yPos),Math.round(w),Math.round(h));
 	}
 }
